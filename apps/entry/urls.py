e8c71aac1cfb4468e391322d6ccdc46a657dfe7c
김민기 <mocodoco@naver.com>
2024-07-09 15:04:21 +0900

    주소 입력 후 좌표 반환기능 구현 완료

--- apps/entry/urls.py
+++ apps/entry/urls.py
@@ -5,9 +5,8 @@ urlpatterns = [
     path('regions', RegionView.as_view(), name='region'),
     path('residences', ResidenceView.as_view(), name='residence'),
     path('types', TypeView.as_view(), name='type'),
-    path('residences/ap/', AptView.as_view(), name='apartment-options'),
+    path('residences/apt', AptView.as_view(), name='apartment-options'),
     path('residences/officetel', OfficetelView.as_view(), name='officetel-options'),
     path('residences/onetwo', OneTwoView.as_view(), name='onetwo-options'),
     path('residences/house', HouseView.as_view(), name='house-options'),
-
 ]
