5b91280ef5754d69017346269de489d5def2cbe5
LilyS222 <soomin.25641@gmail.com>
2024-07-10 17:58:30 +0900

    로그아웃 수정

---
+++ jp.py
@@ -0,0 +1,16 @@
+import requests, json
+
+def get_location(address):
+  url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + address
+  # 'KaKaoAK '는 그대로 두시고 개인키만 지우고 입력해 주세요.
+  # ex) KakaoAK 6af8d4826f0e56c54bc794fa8a294
+  headers = {"Authorization": "KakaoAK b3d155abb4d9e2d9a6814301d72c84d6"}
+  api_json = json.loads(str(requests.get(url,headers=headers).text))
+  address = api_json['documents'][0]['address']
+  crd = {"lat": str(address['y']), "lng": str(address['x'])}
+  address_name = address['address_name']
+
+  return crd
+
+crd = get_location(" ")
+print(crd)
