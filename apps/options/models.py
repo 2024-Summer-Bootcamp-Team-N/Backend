e9dde94d3a168e64686d59813ab3f7ebbe320c28
LilyS222 <soomin.25641@gmail.com>
2024-07-20 17:58:01 +0900

    feat:2차 크롤링

--- apps/options/models.py
+++ apps/options/models.py
@@ -4,7 +4,23 @@ class RoomInfo(models.Model):
     room_info = models.TextField()
     price = models.TextField()
     link = models.URLField()
-
     def __str__(self):
         return self.room_info
 
+class RoomDetailInfo(models.Model):
+    room = models.OneToOneField(RoomInfo, on_delete=models.CASCADE, related_name='details')
+    location = models.TextField()
+    exclusive_overall_area = models.TextField()
+    maintenance_fee = models.TextField()
+    room_type = models.TextField()
+    num_rooms_bathrooms = models.TextField()
+    floor_building_floors = models.TextField()
+    direction = models.TextField()
+    parking_availability = models.TextField()
+    total_parking_spaces = models.TextField()
+    heating_type = models.TextField()
+    building_use = models.TextField()  # 건축물용도
+    move_in_date = models.TextField()
+    approval_date = models.TextField()
+    initial_registration_date = models.TextField()
+    image_url = models.URLField()
--- apps/options/urls.py
+++ apps/options/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GenerateAndCrawlView
+from .views import GenerateAndCrawlView, DetailedRoomInfoView
 
 urlpatterns = [
-    path('crawling', GenerateAndCrawlView.as_view(), name='crawling'),
+    path('crawling/', GenerateAndCrawlView.as_view(), name='trigger_crawl'),
+    path('crawling/<int:room_id>/', DetailedRoomInfoView.as_view(), name='crawl_room_detail'),
 ]
